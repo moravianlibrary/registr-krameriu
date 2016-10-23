131f1a9854203474ac164f1242e21a185be25f84
honza-rychtar <honza.rychtar@gmail.com>
2016-10-23 16:39:01 +0200

    Add Capistrano reset task

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,4 +26,15 @@ namespace :deploy do
       end
     end
   end
+
+  task :reset do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env)  do
+          execute :rake, 'db:reset'
+        end
+      end
+    end
+  end
+
 end
