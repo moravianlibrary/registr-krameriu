14d033940f2c0045cfe75ed09a9d3119831a3f4d
Jan Rychtar <honza.rychtar@gmail.com>
2017-09-12 17:00:52 +0200

    Fix bug with destroy library dependency

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,8 +1,8 @@
 class Library < ActiveRecord::Base
   before_save :refine_url
 	before_save :normalize_blank_values
-  has_many :records
-  
+  has_many :records, dependent: :destroy
+
   has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
 
