f3230e811cc032840cadb5c4c267fc0ac78ee200
Jan Rychtar <honza.rychtar@gmail.com>
2022-06-27 17:37:29 +0200

    Seznam knihoven pres API jen se stavem

--- app/controllers/api/libraries_controller.rb
+++ app/controllers/api/libraries_controller.rb
@@ -3,39 +3,46 @@ class Api::LibrariesController < Api::ApiController
   def index
     result = []
     Library.all.each do |library|
-      item = {
-        id: library.id,
-        code: library.code,
-        sigla: library.sigla,
-        name: library.name,
-        name_en: library.name_en,
-        alive: !!library.alive,
-        version: library.version,
-        url: library.url,
-        new_client_url: library.new_client_url,
-        new_client_version: library.new_client_version,
-        logo: library.logo? ? logo_library_url(library) : nil,
-        email: library.email,
-        web: library.web,
-        street: library.street,
-        city: library.city,
-        zip: library.zip,
-        latitude: library.latitude,
-        longitude: library.longitude,
-        collections: library.collections,
-        recommended_all: library.recommended,
-        recommended_public: library.recommended_public,
-        documents_all: library.documents_all,
-        documents_public: library.documents_public,
-        pages_all: library.pages_all,
-        pages_public: library.pages_public,
-        created_at: library.created_at,
-        updated_at: library.updated_at,
-        last_document_at: library.last_document_at,
-        last_document_before: library.last_document_at.nil? ? nil : ((Time.now.to_date - library.last_document_at.to_date).to_i),
-        licenses: library.licenses.nil? ? [] : JSON.parse(library.licenses)
-      }
-      add_models(item, library)
+      if params[:detail] == "status"
+        item = {
+          code: library.code,
+          alive: !!library.alive
+        }
+      else 
+        item = {
+          id: library.id,
+          code: library.code,
+          sigla: library.sigla,
+          name: library.name,
+          name_en: library.name_en,
+          alive: !!library.alive,
+          version: library.version,
+          url: library.url,
+          new_client_url: library.new_client_url,
+          new_client_version: library.new_client_version,
+          logo: library.logo? ? logo_library_url(library) : nil,
+          email: library.email,
+          web: library.web,
+          street: library.street,
+          city: library.city,
+          zip: library.zip,
+          latitude: library.latitude,
+          longitude: library.longitude,
+          collections: library.collections,
+          recommended_all: library.recommended,
+          recommended_public: library.recommended_public,
+          documents_all: library.documents_all,
+          documents_public: library.documents_public,
+          pages_all: library.pages_all,
+          pages_public: library.pages_public,
+          created_at: library.created_at,
+          updated_at: library.updated_at,
+          last_document_at: library.last_document_at,
+          last_document_before: library.last_document_at.nil? ? nil : ((Time.now.to_date - library.last_document_at.to_date).to_i),
+          licenses: library.licenses.nil? ? [] : JSON.parse(library.licenses)
+        }
+        add_models(item, library)
+      end
       result << item
     end
     render json: result
